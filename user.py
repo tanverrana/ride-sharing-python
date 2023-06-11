a3e3b0863100358f5cece1e45e1ac1931c981772
pro <pro@prodeMacBook-Pro-2.local>
2023-06-11 10:18:45 +0600

    create ride and datatime with fare cost

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from unicodedata import name
+from datetime import datetime
 
 
 class User(ABC):
@@ -19,6 +19,7 @@ class User(ABC):
 class Rider(User):
     def __init__(self, name, email, nid) -> None:
         self.current_ride = None
+        self.wallet = 0
         super().__init__(name, email, nid)
 
     def display_profile(self):
@@ -35,9 +36,36 @@ class Driver(User):
     def __init__(self, name, email, nid, current_location) -> None:
         super().__init__(name, email, nid)
         self.current_location = current_location
+        self.wallet = 0
 
     def display_profile(self):
         print(f'Rider : with name: {self.name} with email:{self.email}')
 
+    def load_cash(self, amount):
+        if amount > 0:
+            self.wallet += amount
+
     def accept_ride(self, ride):
         ride.set_Driver(self)
+
+
+class Ride:
+    def __init__(self, start_location, end_location) -> None:
+        self.start_location = start_location
+        self.end_location = end_location
+        self.driver = None
+        self.rider = None
+        self.start_time = None
+        self.end_time = None
+        self.estimated_fare = None
+
+    def set_driver(self, driver):
+        self.driver = driver
+
+    def start_ride(self):
+        self.start_time = datetime.now()
+
+    def end_ride(self, amount):
+        self.end_time = datetime.now()
+        self.rider.wallet -= self.estimated_fare
+        self.driver.wallet += self.estimated_fare
